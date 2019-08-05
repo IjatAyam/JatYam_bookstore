562cfd8e46e9ebb59896bd9aa1922d705ddcd8e7
Ijat Ayam <ijat191999@gmail.com>
2019-08-06 00:01:52 +0800

    add views, templates and statics files

--- shop/models.py
+++ shop/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("shop:product_list_by_category", args=[self.slug])
+        return reverse("shop:product_list_by_category", kwargs={"category_slug": self.slug})
 
 
 class Product(models.Model):
@@ -41,4 +41,4 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("shop:product_detail", args=[self.id, self.slug])
+        return reverse("shop:product_detail", kwargs={"pk": self.pk, "slug": self.slug})
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+from .models import Product, Category
+
+
+class ProductListView(ListView):
+    model = Product
+    context_object_name = "products"
+
+    def get_queryset(self):
+        products = super().get_queryset().filter(available=True)
+        if 'category_slug' in self.kwargs:
+            category = get_object_or_404(
+                Category, slug=self.kwargs['category_slug'])
+            products = products.filter(category=category)
+        return products
+
+    def get_context_data(self, **kwargs):
+        # Call base context
+        context = super().get_context_data(**kwargs)
+        # Add catogories
+        context['category'] = None
+        context['categories'] = Category.objects.all()
+        if 'category_slug' in self.kwargs:
+            context['category'] = Category.objects.get(
+                slug=self.kwargs['category_slug'])
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+
+    def get_queryset(self):
+        product = super().get_queryset()
+        try:
+            product.get(slug=self.kwargs['slug'])
+        except:
+            raise Http404('No product found matching the query')
+        return product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
