ca4ef900e7320002fcf02fcc744b6c4d4a162683
Ijat Ayam <ijat191999@gmail.com>
2019-08-17 18:06:24 +0800

    change db to postgres

--- JatYam_bookstore/settings.py
+++ JatYam_bookstore/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'JatYam_bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookstore',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
