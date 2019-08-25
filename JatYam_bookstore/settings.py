09bdbc745657a0a01c35dce29ac709f8d8b0b71e
Ijat Ayam <ijat191999@gmail.com>
2019-08-26 01:10:08 +0800

    add aws S3

--- JatYam_bookstore/settings.py
+++ JatYam_bookstore/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'coupons.apps.CouponsConfig',
     'wishlist.apps.WishlistConfig',
     'orders.apps.OrdersConfig',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -150,3 +151,13 @@ WISHLIST_SESSION_ID = 'wishlist'
 
 # django heroku
 django_heroku.settings(locals())
+
+# AWS settings
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
