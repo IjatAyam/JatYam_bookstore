267a18a221af4590e19267b02b928af756fac2e7
Ijat Ayam <ijat191999@gmail.com>
2019-08-25 17:54:54 +0800

    settings static root

--- JatYam_bookstore/settings.py
+++ JatYam_bookstore/settings.py
@@ -128,11 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
 # for web media
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # for send email
 EMAIL_HOST = 'smtp.gmail.com'
